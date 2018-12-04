2ef38a63fc6709f736311a08d75bbbffc7fdbf35
Cupertino Miranda <cmiranda@synopsys.com>
2018-12-04 22:24:00 +0000

    Removed old fashion aux-reg mapping.

--- target/arc/op_helper.c
+++ target/arc/op_helper.c
@@ -25,45 +25,6 @@
 #include "translate-all.h"
 #include "arc-regs.h"
 
-#define AUX_ID_STATUS           0x000
-#define AUX_ID_SEMAPHORE        0x001
-#define AUX_ID_LP_START         0x002
-#define AUX_ID_LP_END           0x003
-#define AUX_ID_IDENTITY         0x004
-#define AUX_ID_DEBUG            0x005
-#define AUX_ID_PC               0x006
-
-#define AUX_ID_STATUS32         0x00A
-#define AUX_ID_STATUS32_L1      0x00B
-#define AUX_ID_STATUS32_L2      0x00C
-
-#define AUX_ID_MULHI            0x012
-
-#define AUX_ID_INT_VECTOR_BASE  0x025
-
-#define AUX_ID_INT_MACMODE      0x041
-
-#define AUX_ID_IRQ_LV12         0x043
-
-#define AUX_ID_IRQ_LEV          0x200
-#define AUX_ID_IRQ_HINT         0x201
-
-#define AUX_ID_ERET             0x400
-#define AUX_ID_ERBTA            0x401
-#define AUX_ID_ERSTATUS         0x402
-#define AUX_ID_ECR              0x403
-#define AUX_ID_EFA              0x404
-
-#define AUX_ID_ICAUSE1          0x40A
-#define AUX_ID_ICAUSE2          0x40B
-#define AUX_ID_IENABLE          0x40C
-#define AUX_ID_ITRIGGER         0x40D
-
-#define AUX_ID_BTA              0x412
-#define AUX_ID_BTA_L1           0x413
-#define AUX_ID_BTA_L2           0x414
-#define AUX_ID_IRQ_PULSE_CANSEL 0x415
-#define AUX_ID_IRQ_PENDING      0x416
 
 //target_ulong helper_norm(CPUARCState *env, uint32_t src1)
 //{
@@ -137,42 +98,42 @@ target_ulong helper_fls(CPUARCState *env, uint32_t src)
 void helper_sr(CPUARCState *env, uint32_t val, uint32_t aux)
 {
     switch (arc_aux_reg_struct_for_address(aux, ARC_OPCODE_ARCv2HS)->id) {
-        case AUX_ID_STATUS: {
+        case AUX_ID_status: {
         } break;
 
-        case AUX_ID_SEMAPHORE: {
+        case AUX_ID_semaphore: {
         } break;
 
-        case AUX_ID_LP_START: {
+        case AUX_ID_lp_start: {
 	  env->lps = val;
         } break;
 
-        case AUX_ID_LP_END: {
+        case AUX_ID_lp_end: {
 	  env->lpe = val;
         } break;
 
-        case AUX_ID_IDENTITY: {
+        case AUX_ID_identity: {
         } break;
 
-        case AUX_ID_DEBUG: {
+        case AUX_ID_debug: {
         } break;
 
-        case AUX_ID_PC: {
+        case AUX_ID_pc: {
         } break;
 
-        case AUX_ID_STATUS32: {
+        case AUX_ID_status32: {
         } break;
 
-        case AUX_ID_STATUS32_L1: {
+        case AUX_ID_status32_l1: {
         } break;
 
-        case AUX_ID_STATUS32_L2: {
+        case AUX_ID_status32_l2: {
         } break;
 
-        case AUX_ID_MULHI: {
+        case AUX_ID_mulhi: {
         } break;
 
-        case AUX_ID_INT_VECTOR_BASE: {
+        case AUX_ID_int_vector_base: {
         } break;
 
         case AUX_ID_aux_macmode: {
