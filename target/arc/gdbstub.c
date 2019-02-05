017896b39a1dfe20ea2abe39bbfd54b9af1f6d28
Shahab Vahedi <shahab@synopsys.com>
2019-02-05 15:02:06 +0100

    [GDB] report status32 fully.

--- target/arc/gdbstub.c
+++ target/arc/gdbstub.c
@@ -23,6 +23,7 @@
 #include "qemu-common.h"
 #include "exec/gdbstub.h"
 #include "internals.h"
+#include "irq.h"
 
 
 int arc_cpu_gdb_read_register(CPUState *cs, uint8_t *mem_buf, int n)
@@ -60,7 +61,7 @@ int arc_cpu_gdb_write_register(CPUState *cs, uint8_t *mem_buf, int n)
 {
   ARCCPU *cpu = ARC_CPU(cs);
   CPUARCState *env = &cpu->env;
-  uint16_t regval = ldl_p(mem_buf);
+  uint32_t regval = ldl_p(mem_buf);
 
   switch (n)
   {
@@ -87,38 +88,6 @@ int arc_cpu_gdb_write_register(CPUState *cs, uint8_t *mem_buf, int n)
 }
 
 
-static uint32_t arc_cpu_get_stat32(CPUARCState *env)
-{
-    uint32_t val = 0;
-    val |= env->stat.Hf  ? BIT(0)  : 0;
-    val |= env->stat.Ef << 1;
-    val |= env->stat.AEf ? BIT(5)  : 0;
-    val |= env->stat.DEf ? BIT(6)  : 0;
-    val |= env->stat.Uf  ? BIT(7)  : 0;
-    val |= env->stat.Vf  ? BIT(8)  : 0;
-    val |= env->stat.Cf  ? BIT(9)  : 0;
-    val |= env->stat.Nf  ? BIT(10) : 0;
-    val |= env->stat.Zf  ? BIT(11) : 0;
-    val |= env->stat.Lf  ? BIT(12) : 0;
-    return val;
-}
-
-
-static void arc_cpu_set_stat32(CPUARCState *env, uint32_t val)
-{
-    env->stat.Hf  = 0 != (val & BIT(0));
-    env->stat.Ef  = (val >> 1) & 0x0f;
-    env->stat.AEf = 0 != (val & BIT(5));
-    env->stat.DEf = 0 != (val & BIT(6));
-    env->stat.Uf  = 0 != (val & BIT(7));
-    env->stat.Vf  = 0 != (val & BIT(8));
-    env->stat.Cf  = 0 != (val & BIT(9));
-    env->stat.Nf  = 0 != (val & BIT(10));
-    env->stat.Zf  = 0 != (val & BIT(11));
-    env->stat.Lf  = 0 != (val & BIT(12));
-}
-
-
 static int
 arc_aux_minimal_gdb_get_reg(CPUARCState *env, uint8_t *mem_buf, int regnum)
 {
@@ -135,7 +104,7 @@ arc_aux_minimal_gdb_get_reg(CPUARCState *env, uint8_t *mem_buf, int regnum)
       regval = env->lpe;
       break;
     case GDB_AUX_REG_STATUS:
-      regval = arc_cpu_get_stat32(env);
+      regval = pack_status32(&env->stat);
       break;
     default:
       assert(!"Unsupported auxiliary register is being read.");
@@ -160,7 +129,7 @@ arc_aux_minimal_gdb_set_reg(CPUARCState *env, uint8_t *mem_buf, int regnum)
       env->lpe = regval;
       break;
     case GDB_AUX_REG_STATUS:
-      arc_cpu_set_stat32(env, regval);
+      unpack_status32(&env->stat, regval);
       break;
     default:
       assert(!"Unsupported auxiliary register is being written.");
