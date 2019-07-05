b220221c70fd81a1c92cc02b9ba6baa5149150dc
Shahab Vahedi <shahab@synopsys.com>
2019-07-05 16:55:58 +0200

    [FORMAT] Make op_helper.c QEMU style
    Signed-off-by: Shahab Vahedi <shahab@synopsys.com>

--- target/arc/op_helper.c
+++ target/arc/op_helper.c
@@ -33,117 +33,128 @@
 #include "sysemu/sysemu.h"
 
 
-static target_ulong get_status32 (CPUARCState *env)
+static target_ulong get_status32(CPUARCState *env)
 {
-  target_ulong value = pack_status32 (&env->stat);
+    target_ulong value = pack_status32 (&env->stat);
 
-  /* TODO: Implement debug mode */
-  if(env->stat.Uf == 1)
-    {
-      value &= 0x00000f00;
+    /* TODO: Implement debug mode */
+    if (env->stat.Uf == 1) {
+        value &= 0x00000f00;
     }
 
-  if (env->stopped)
-    value |= BIT(0);
+    if (env->stopped) {
+        value |= BIT(0);
+    }
 
-  return value;
+    return value;
 }
 
 static void set_status32(CPUARCState *env, target_ulong value)
 {
-  ARCCPU *cpu = arc_env_get_cpu(env);
-  /* TODO: Implement debug mode. */
-  bool debug_mode = false;
-  if(env->stat.Uf == 1)
-    {
-      value &= 0x00000f00;
+    ARCCPU *cpu = arc_env_get_cpu(env);
+    /* TODO: Implement debug mode. */
+    bool debug_mode = false;
+    if (env->stat.Uf == 1) {
+        value &= 0x00000f00;
     }
-  else if(!debug_mode)
-    {
-      value &= 0xffff6f3f;
+    else if (!debug_mode) {
+        value &= 0xffff6f3f;
     }
 
-  if(((env->stat.Uf >> 7)  & 0x1) != ((value >> 7)  & 0x1))
-    tlb_flush(CPU(cpu));
+    if (((env->stat.Uf >> 7)  & 0x1) != ((value >> 7)  & 0x1)) {
+        tlb_flush(CPU(cpu));
+    }
 
-  unpack_status32(&env->stat, value);
+    unpack_status32(&env->stat, value);
 
-  /* Implement HALT functionality.  */
-  if (value & 0x01)
-    qemu_system_shutdown_request(SHUTDOWN_CAUSE_GUEST_SHUTDOWN);
+    /* Implement HALT functionality.  */
+    if (value & 0x01) {
+        qemu_system_shutdown_request(SHUTDOWN_CAUSE_GUEST_SHUTDOWN);
+    }
 }
 
-static void
-do_exception_no_delayslot (CPUARCState *env, uint32_t index, uint32_t causecode, uint32_t param)
+static void do_exception_no_delayslot(CPUARCState *env, uint32_t index,
+                                      uint32_t causecode, uint32_t param)
 {
-  ARCCPU *cpu = arc_env_get_cpu(env);
-  CPUState *cs = CPU(cpu);
-  cpu_restore_state(cs, GETPC(), true);
-  env->eret = env->pc;
-  env->erbta = env->npc_helper;
+    ARCCPU *cpu = arc_env_get_cpu(env);
+    CPUState *cs = CPU(cpu);
+    cpu_restore_state(cs, GETPC(), true);
+    env->eret = env->pc;
+    env->erbta = env->npc_helper;
 
-  helper_raise_exception (env, index, causecode, param);
+    helper_raise_exception (env, index, causecode, param);
 }
 
 target_ulong helper_norm(CPUARCState *env, uint32_t src1)
 {
-  int i;
-  int32_t tmp = (int32_t) src1;
-  if(tmp == 0 || tmp == -1)
-    return 0;
-  for(i = 0; i <= 31; i++)
-  {
-    if((tmp >> i) == 0) break;
-    if((tmp >> i) == -1) break;
-  }
-  return i;
+    int i;
+    int32_t tmp = (int32_t) src1;
+    if (tmp == 0 || tmp == -1) {
+      return 0;
+    }
+    for (i = 0; i <= 31; i++) {
+      if ((tmp >> i) == 0) {
+          break;
+      }
+      if ((tmp >> i) == -1) {
+          break;
+      }
+    }
+    return i;
 }
 
 target_ulong helper_normh(CPUARCState *env, uint32_t src1)
 {
-  int i;
-  for(i = 0; i <= 15; i++)
-  {
-    if(src1 >> i == 0) break;
-    if(src1 >> i == -1) break;
-  }
-  return i;
+    int i;
+    for (i = 0; i <= 15; i++) {
+      if (src1 >> i == 0) {
+          break;
+      }
+      if (src1 >> i == -1) {
+          break;
+      }
+    }
+    return i;
 }
 
 target_ulong helper_ffs(CPUARCState *env, uint32_t src)
 {
-  int i;
-  if(src == 0)
-    return 31;
-  for(i = 0; i <= 31; i++)
-    if(((src >> i) & 1) != 0)
-      break;
-  return i;
+    int i;
+    if (src == 0) {
+      return 31;
+    }
+    for (i = 0; i <= 31; i++) {
+      if (((src >> i) & 1) != 0) {
+        break;
+      }
+    }
+    return i;
 }
 
 target_ulong helper_fls(CPUARCState *env, uint32_t src)
 {
-  int i;
-  if(src == 0)
-    return 0;
-
-  for(i = 31; i >= 0; i--)
-    if(((src >> i) & 1) != 0)
-      break;
-  return i;
-}
-
-static void
-report_aux_reg_error(uint32_t aux)
-{
-  if (((aux >= ARC_BCR1_START) && (aux <= ARC_BCR1_END)) ||
-      ((aux >= ARC_BCR2_START) && (aux <= ARC_BCR2_END)))
-    {
-      qemu_log_mask(LOG_UNIMP, "Undefined BCR 0x%03x\n", aux);
+    int i;
+    if (src == 0) {
+      return 0;
     }
 
-  error_report("Undefined AUX register 0x%03x, aborting", aux);
-  exit(EXIT_FAILURE);
+    for (i = 31; i >= 0; i--) {
+      if (((src >> i) & 1) != 0) {
+        break;
+      }
+    }
+    return i;
+}
+
+static void report_aux_reg_error(uint32_t aux)
+{
+    if (((aux >= ARC_BCR1_START) && (aux <= ARC_BCR1_END)) ||
+        ((aux >= ARC_BCR2_START) && (aux <= ARC_BCR2_END))) {
+        qemu_log_mask(LOG_UNIMP, "Undefined BCR 0x%03x\n", aux);
+    }
+
+    error_report("Undefined AUX register 0x%03x, aborting", aux);
+    exit(EXIT_FAILURE);
 }
 
 void helper_sr(CPUARCState *env, uint32_t val, uint32_t aux)
@@ -207,19 +218,19 @@ void helper_sr(CPUARCState *env, uint32_t val, uint32_t aux)
 
 static target_ulong get_debug(CPUARCState *env)
 {
-  target_ulong res = 0x00000000;
+    target_ulong res = 0x00000000;
 
-  res |= (env->debug.LD) ? BIT(31) : 0;
-  res |= (env->debug.SH) ? BIT(30) : 0;
-  res |= (env->debug.BH) ? BIT(29) : 0;
-  res |= (env->debug.UB) ? BIT(28) : 0;
-  res |= (env->debug.ZZ) ? BIT(27) : 0;
-  res |= (env->debug.RA) ? BIT(22) : 0;
-  res |= (env->debug.IS) ? BIT(11) : 0;
-  res |= (env->debug.FH) ? BIT(1)  : 0;
-  res |= (env->debug.SS) ? BIT(0)  : 0;
+    res |= (env->debug.LD) ? BIT(31) : 0;
+    res |= (env->debug.SH) ? BIT(30) : 0;
+    res |= (env->debug.BH) ? BIT(29) : 0;
+    res |= (env->debug.UB) ? BIT(28) : 0;
+    res |= (env->debug.ZZ) ? BIT(27) : 0;
+    res |= (env->debug.RA) ? BIT(22) : 0;
+    res |= (env->debug.IS) ? BIT(11) : 0;
+    res |= (env->debug.FH) ? BIT(1)  : 0;
+    res |= (env->debug.SS) ? BIT(0)  : 0;
 
-  return res;
+    return res;
 }
 
 static target_ulong get_identity(CPUARCState *env)
@@ -227,21 +238,21 @@ static target_ulong get_identity(CPUARCState *env)
     target_ulong chipid = 0xffff, arcnum = 0, arcver, res;
 
     switch (env->family) {
-	case ARC_OPCODE_ARC700: {
+	case ARC_OPCODE_ARC700:
 	    arcver = 0x34;
-	} break;
+	    break;
 
-	case ARC_OPCODE_ARCv2EM: {
+	case ARC_OPCODE_ARCv2EM:
 	    arcver = 0x44;
-	} break;
+	    break;
 
-	case ARC_OPCODE_ARCv2HS: {
+	case ARC_OPCODE_ARCv2HS:
 	    arcver = 0x54;
-	} break;
+	    break;
 
-	default: {
+	default:
 	    arcver = 0;
-	}
+
     }
 
     // TODO: in SMP, arcnum depends on the cpu instance
@@ -256,7 +267,7 @@ target_ulong helper_lr(CPUARCState *env, uint32_t aux)
     struct arc_aux_reg_detail *aux_reg_detail =
         arc_aux_reg_struct_for_address(aux, ARC_OPCODE_ARCv2HS);
 
-    if(aux_reg_detail == NULL) {
+    if (aux_reg_detail == NULL) {
         report_aux_reg_error(aux);
     }
 
@@ -336,9 +347,9 @@ target_ulong helper_lr(CPUARCState *env, uint32_t aux)
         break;
 
     default:
-        if(aux_reg_detail->aux_reg->get_func != NULL) {
-            result = aux_reg_detail->aux_reg->get_func (aux_reg_detail,
-                                                        (void *) env);
+        if (aux_reg_detail->aux_reg->get_func != NULL) {
+            result = aux_reg_detail->aux_reg->get_func(aux_reg_detail,
+                                                       (void *) env);
         }
         else {
             /* TODO: is lr and sr possible delayslot instructions ? */
@@ -354,152 +365,148 @@ target_ulong helper_lr(CPUARCState *env, uint32_t aux)
 
 void QEMU_NORETURN helper_halt(CPUARCState *env)
 {
-  CPUState *cs = CPU (arc_env_get_cpu (env));
-  if (env->stat.Uf)
-    {
-      cs->exception_index = EXCP_PRIVILEGEV;
-      env->causecode = 0;
-      env->param = 0;
-       /* Restore PC such that we point at the faulty instruction.  */
-      env->eret = env->pc;
+    CPUState *cs = CPU(arc_env_get_cpu(env));
+    if (env->stat.Uf) {
+        cs->exception_index = EXCP_PRIVILEGEV;
+        env->causecode = 0;
+        env->param = 0;
+         /* Restore PC such that we point at the faulty instruction.  */
+        env->eret = env->pc;
+    } else {
+        env->pc = env->npc_helper;
+        cs->halted = 1;
+        cs->exception_index = EXCP_HLT;
     }
-  else
-    {
-      env->pc = env->npc_helper;
-      cs->halted = 1;
-      cs->exception_index = EXCP_HLT;
-    }
-  cpu_loop_exit (cs);
+    cpu_loop_exit(cs);
 }
 
-void helper_rtie (CPUARCState *env)
+void helper_rtie(CPUARCState *env)
 {
-  CPUState *cs = CPU (arc_env_get_cpu (env));
-  if (env->stat.Uf)
-    {
-      cs->exception_index = EXCP_PRIVILEGEV;
-      env->causecode = 0;
-      env->param = 0;
-       /* Restore PC such that we point at the faulty instruction.  */
-      env->eret = env->pc;
-      cpu_loop_exit (cs);
-      return;
+    CPUState *cs = CPU(arc_env_get_cpu(env));
+    if (env->stat.Uf) {
+        cs->exception_index = EXCP_PRIVILEGEV;
+        env->causecode = 0;
+        env->param = 0;
+         /* Restore PC such that we point at the faulty instruction.  */
+        env->eret = env->pc;
+        cpu_loop_exit(cs);
+        return;
     }
 
-  if (env->stat.AEf || (env->aux_irq_act & 0xFFFF) == 0)
-    {
-      assert (env->stat.Uf == 0);
-      CPU_PCL(env) = env->eret;
-      env->stat = env->stat_er;
-      env->bta = env->erbta;
+    if (env->stat.AEf || (env->aux_irq_act & 0xFFFF) == 0) {
+        assert (env->stat.Uf == 0);
+        CPU_PCL(env) = env->eret;
+        env->stat = env->stat_er;
+        env->bta = env->erbta;
 
-      /* If returning to userland, restore SP.  */
-      if (env->stat.Uf)
-        switchSP (env);
+        /* If returning to userland, restore SP.  */
+        if (env->stat.Uf) {
+            switchSP(env);
+        }
 
-      qemu_log_mask(CPU_LOG_INT, "[EXCP] RTIE @0x%08x ECR:0x%08x\n",
-                    env->r[63], env->ecr);
-    }
-  else
-    {
-      arc_rtie_interrupts (env);
-      qemu_log_mask(CPU_LOG_INT, "[IRQ] RTIE @0x%08x STATUS32:0x%08x\n",
-                    env->r[63], pack_status32 (&env->stat));
+        qemu_log_mask(CPU_LOG_INT, "[EXCP] RTIE @0x%08x ECR:0x%08x\n",
+                      env->r[63], env->ecr);
+    } else {
+        arc_rtie_interrupts (env);
+        qemu_log_mask(CPU_LOG_INT, "[IRQ] RTIE @0x%08x STATUS32:0x%08x\n",
+                      env->r[63], pack_status32 (&env->stat));
     }
 }
 
 void helper_flush(CPUARCState *env)
 {
-  tb_flush((CPUState *)arc_env_get_cpu(env));
+    tb_flush((CPUState *)arc_env_get_cpu(env));
 }
 
 /* This should only be called from translate, via gen_raise_exception.
    We expect that ENV->PC has already been updated.  */
 
-void QEMU_NORETURN
-helper_raise_exception (CPUARCState *env,
-			uint32_t index,
-			uint32_t causecode,
-			uint32_t param)
+void QEMU_NORETURN helper_raise_exception(CPUARCState *env,
+			                              uint32_t index,
+			                              uint32_t causecode,
+			                              uint32_t param)
 {
-  ARCCPU *cpu = arc_env_get_cpu(env);
-  CPUState *cs = CPU (cpu);
-  // Cannot restore state here.
-  //cpu_restore_state(cs, GETPC(), true);
-  cs->exception_index = index;
-  env->causecode = causecode;
-  env->param = param;
-  cpu_loop_exit(cs);
+    ARCCPU *cpu = arc_env_get_cpu(env);
+    CPUState *cs = CPU(cpu);
+    /* Cannot restore state here. */
+    /* cpu_restore_state(cs, GETPC(), true); */
+    cs->exception_index = index;
+    env->causecode = causecode;
+    env->param = param;
+    cpu_loop_exit(cs);
 }
 
 uint32_t helper_get_status32(CPUARCState *env)
 {
-  return get_status32(env);
+    return get_status32(env);
 }
 
 void helper_set_status32(CPUARCState *env, uint32_t value)
 {
-  set_status32(env, value);
+    set_status32(env, value);
 }
 
 uint32_t helper_carry_add_flag(uint32_t dest, uint32_t b, uint32_t c)
 {
-  uint32_t t1, t2, t3;
+    uint32_t t1, t2, t3;
 
-  t1 = b & c;
-  t2 = b & (~dest);
-  t3 = c & (~dest);
-  t1 = t1 | t2 | t3;
-  return (t1 >> 31) & 1;
+    t1 = b & c;
+    t2 = b & (~dest);
+    t3 = c & (~dest);
+    t1 = t1 | t2 | t3;
+    return (t1 >> 31) & 1;
 }
 
 uint32_t helper_overflow_add_flag(uint32_t dest, uint32_t b, uint32_t c)
 {
-  dest >>= 31;
-  b >>= 31;
-  c >>= 31;
-  if((dest == 0 && b == 1 && c == 1)
-     || (dest == 1 && b == 0 && c == 0))
-    return 1;
-  else
-    return 0;
+    dest >>= 31;
+    b >>= 31;
+    c >>= 31;
+    if ((dest == 0 && b == 1 && c == 1)
+        || (dest == 1 && b == 0 && c == 0)) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 uint32_t helper_overflow_sub_flag(uint32_t dest, uint32_t b, uint32_t c)
 {
-  dest >>= 31;
-  b >>= 31;
-  c >>= 31;
-  if((dest == 1 && b == 0 && c == 1)
-     || (dest == 0 && b == 1 && c == 0))
-    return 1;
-  else
-    return 0;
+    dest >>= 31;
+    b >>= 31;
+    c >>= 31;
+    if ((dest == 1 && b == 0 && c == 1)
+        || (dest == 0 && b == 1 && c == 0)) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 uint32_t helper_repl_mask (uint32_t dest, uint32_t src, uint32_t mask)
 {
-  uint32_t ret = dest & (~mask);
-  ret |= (src & mask);
+    uint32_t ret = dest & (~mask);
+    ret |= (src & mask);
 
-  return ret;
+    return ret;
 }
 
 uint32_t helper_mpymu(CPUARCState *env, uint32_t b, uint32_t c)
 {
-  uint64_t _b = (uint64_t) b;
-  uint64_t _c = (uint64_t) c;
+    uint64_t _b = (uint64_t) b;
+    uint64_t _c = (uint64_t) c;
 
-  return (uint32_t) ((_b * _c) >> 32);
+    return (uint32_t) ((_b * _c) >> 32);
 }
 
 uint32_t helper_mpym(CPUARCState *env, uint32_t b, uint32_t c)
 {
-  int64_t _b = (int64_t) ((int32_t) b);
-  int64_t _c = (int64_t) ((int32_t) c);
+    int64_t _b = (int64_t) ((int32_t) b);
+    int64_t _c = (int64_t) ((int32_t) c);
 
-//  fprintf(stderr, "B = 0x%llx, C = 0x%llx, result = 0x%llx\n", _b, _c, _b * _c);
-  return ((_b * _c) >> 32);
+    /* fprintf(stderr, "B = 0x%llx, C = 0x%llx, result = 0x%llx\n",
+     *         _b, _c, _b * _c); */
+    return ((_b * _c) >> 32);
 }
 
 
