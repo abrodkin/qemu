3c0d42143f4351c8d49ef3248fb9f32e9cf1aac8
Cupertino Miranda <cmiranda@synopsys.com>
2020-03-23 12:19:59 +0000

    Removed assert on DEf flag.

--- target/arc/irq.c
+++ target/arc/irq.c
@@ -471,12 +471,6 @@ bool arc_cpu_exec_interrupt (CPUState *cs, int interrupt_request)
   uint32_t vectno = 0;
   uint32_t offset, priority;
 
-  /* TODO: Check if we are in a delay slot.  We may need to avoid
-     executing interrupts when in delay slots.	*/
-  if (env->stat.DEf) {
-    assert(!"An interrupt is raised during a delay slot.");
-  }
-
   /* Check if we should execute this interrupt.	 */
   if (env->stat.Hf
       /* The interrupts are enabled.  */
